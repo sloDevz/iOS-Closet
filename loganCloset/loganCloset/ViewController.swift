967dddd96264c3a27a8f5279682cf55aefe806f1
sloDevz <onec555@gmail.com>
2023-05-18 14:08:58 +0900

    Xcode 프로젝트 생성

---
+++ loganCloset/loganCloset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loganCloset
+//
+//  Created by DONGWOOK SEO on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
